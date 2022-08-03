6386f0c75d46162f8afb03696bb07a1e28d9f010
dskim <ektha115@gmail.com>
2022-08-03 17:58:00 +0900

    이벤트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Header(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <h1><a href="/" onClick={(event) => {
+        event.preventDefault();
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
+    </header>
+  );
+}
+
+function Nav(props) {
+  const lis = [];
+
+  for(let i = 0; i < props.topics.length; i ++) {
+    let t = props.topics[i];
+    lis.push(
+      <li key={t.id}>
+        <a id={t.id} href={'/read/'+t.id} onClick={(e)=>{
+          e.preventDefault();
+          props.onChangeMode(e.target.id);
+        }}>{t.title}</a>
+      </li>
+    );
+  }
+
+  return (
+    <nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
+  )
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  )
+}
+
+function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'js', body:'js is ...'},
+  ];
+  return (
+    <div>
+      <Header title="WEB" onChangeMode={() => {
+        alert('Header');
+      }} />
+      <Nav topics={topics} onChangeMode={(id) => {
+        alert(id);
+      }}/>
+      <Article title="Welcome" body="Hello, WEB" />
     </div>
   );
 }
